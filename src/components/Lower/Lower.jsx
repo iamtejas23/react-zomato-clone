b8343cffedb4936c368b966091cef77928b04501
Tejas Mane <tsmane8787@gmail.com>
2024-10-01 15:41:39 +0530

    UI updates

--- src/components/Lower/Lower.jsx
+++ src/components/Lower/Lower.jsx
@@ -6,7 +6,7 @@ const Lower = () => {
     <div>
         <div className="lower">
  <hr className='hrline' />
-        <p>By continuing past this page, you agree to our Terms of Service, Cookie Policy, Privacy Policy and Content Policies. All trademarks are properties of their respective owners. 2008-2024 © Zomato™ Ltd. All rights reserved.</p>
+        <p>Created By Tejas Mane All trademarks are properties of their respective owners. 2008-2024 © Zomato™ Ltd. All rights reserved.</p>
         
     </div>
     </div>
