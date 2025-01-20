c45f05e7e9ee85e22f69305775fc61c759d2bbb7
Tejas Mane <60383103+iamtejas23@users.noreply.github.com>
2025-01-21 00:59:02 +0530

    Update Home.jsx

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
     <div className="hero">
       <Navbar /> {/* Include Navbar here */}
       <div className="hero-content">
-        <h1>Fomato</h1>
+        <h1>Fomato V1</h1>
         <p>Discover the best food & drinks near you</p>
         <SearchBar />
       </div>
